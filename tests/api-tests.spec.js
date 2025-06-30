65baad078c0155be7d971ea83e0d3ab0ef211b10
KrzysztofKundys <krzysztofkundys@gmail.com>
2025-06-30 20:23:57 +0200

    finalChanges

--- tests/api-tests.spec.js
+++ tests/api-tests.spec.js
@@ -2,17 +2,23 @@
 import { test, expect } from '@playwright/test';
 import { request } from '@playwright/test';
 
-test('API GET tests', async ({ request })) => {
+test('API GET tests', async ({ request }) => {
     const response = await request.get('api/productsList');
     expect(response).toBe(200);
     const data = await response.json();
     expect(data).toHaveProperty('products');
     expect(data.products.length).toBeGreaterThan(0);
-};
+)};
 
-test('API POST Test', async({ request })) => {
+test('API POST Test', async ({ request }) => {
     const response = await request.post('api/productsList'){
         form: {
-            search_product: 'Sleeves Top and Short - Blue & Pink'
-      }};
-};
+            search_product: 'top'
+        }
+    });
+      expect(response.ok()).toBeTruthy();
+      const data = await response.json();
+      expect(data).toHaveProperty('products');
+      expect(data.products.length).toBeGreaterThan(0);
+      expect(data.products[0].name.toLowerCase()).toContain('top');
+});
